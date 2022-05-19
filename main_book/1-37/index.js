5d2dbdc250c6bf2823ad0d28d24421dd9dc110c8
0603anton <zorgzb@gmail.com>
2022-05-19 10:54:20 +0200

    Основы - done

---
+++ main_book/1-37/index.js
@@ -0,0 +1,122 @@
+"use strict";
+
+/*
+	Вопросы и что повторить обязательно:
+  1) Добавление версий файлам JS - синтаксис, для чего?
+  2) Типы комментариев в JS
+  3) Нельзя использовать несколько let для одной и той же переменной.
+  4) Шаблонные строки в JavaScript
+  5) Преобразование к числу в JavaScript
+  6) Неточные вычисления в JavaScript метод ...
+
+*/
+
+// №1
+// alert("YO");
+// console.log("dskjflhdlk");
+
+// №6
+// let num = 123;
+// alert(num);
+
+// let a = 1,
+//   b = 2,
+//   c = 3;
+
+// let a, b, c;
+// a = 1;
+// b = 2;
+// c = 3;
+
+// let a = 1,
+//   b = 2,
+//   c = 3;
+
+// №7
+
+// let a = 1 + 2 + 3;
+// alert(a);
+
+// №8
+// 8.1
+
+// let a = 10;
+// let b = 2;
+// alert(a + b);
+// alert(a - b);
+// alert(a * b);
+// alert(a / b);
+
+// 8.2
+
+// let c = 10;
+// let d = 5;
+// let result = c + d;
+// alert(result);
+
+// 8.4
+
+// let a = 10;
+// let b = 5;
+// let c = a - b;
+// let d = 7;
+// let result = c + d;
+// alert(result);
+
+// №12.1
+
+// let a = 13;
+// let b = 5;
+// let result = a % b;
+// alert(result);
+
+// #13.1
+
+// alert(2 ** 10);
+
+// #14
+// alert("Anton Kucherov");
+
+// #15 #16
+// let str = "!!!";
+// alert(str.length);
+
+// let str = `a
+// b
+// c`;
+// alert(str);
+
+// #18
+
+// let a;
+// alert(a);
+
+// #23
+
+// let px1 = "5px";
+// let px2 = "6px";
+
+// let sum = parseFloat(px1) + parseFloat(px2);
+// alert(sum + "px");
+
+// #38
+
+// let arr = [`a`, `b`, `c`];
+// console.log(arr);
+
+// №39
+// let obj = {
+//   1: `a`,
+//   2: `b`,
+//   3: `c`,
+// };
+
+// console.log(obj);
+
+// let obj = {
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
+
+// console.log(obj[`c`] + obj[`b`] + obj[`a`]);

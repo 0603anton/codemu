625c0b3692591fe7b877bb622f7a4ea5c9fd20d7
0603anton <zorgzb@gmail.com>
2022-07-08 21:31:19 +0200

    add

--- LRJS/index.js
+++ LRJS/index.js
@@ -574,3 +574,67 @@ let users = [vasya, petya, masha];
 
 let names = users.map((item) => item.name);
 console.log(names); // Вася, Петя, Маша
+
+
+
+Трансформировать в объекты
+// TODO
+важность: 5
+У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+  Например:
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [vasya, petya, masha];
+
+let usersMapped = users.map(item => ({
+  fullName: `${item.name} ${item.surname}`,
+  id: item.id,
+
+
+}))
+
+/*
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+*/
+console.log(usersMapped[0].id)
+console.log(usersMapped[0].fullName)
+alert(usersMapped[0].id) // 1
+alert(usersMapped[0].fullName) // Вася Пупкин
+Итак, на самом деле вам нужно трансформировать один массив объектов в другой.Попробуйте использовать =>.Это небольшая уловка.
+
+
+Отсортировать пользователей по возрасту
+// TODO
+важность: 5
+Напишите функцию sortByAge(users), которая принимает массив объектов со свойством age и сортирует их по нему.
+
+  Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let arr = [vasya, petya, masha];
+
+function sortByAge(arr) {
+  arr.sort((a, b) => {
+    return a.age - b.age
+  })
+}
+
+sortByAge(arr);
+console.log(arr);
+// теперь: [vasya, masha, petya]
+alert(arr[0].name); // Вася
+alert(arr[1].name); // Маша
+alert(arr[2].name); // Петя

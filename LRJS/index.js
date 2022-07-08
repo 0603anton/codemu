bd1fd69c1c5869fe7daa96ccb18d2864718bfab9
0603antone <zorgzb@gmail.com>
2022-07-08 08:40:39 +0200

    08/07/2022

--- LRJS/index.js
+++ LRJS/index.js
@@ -415,3 +415,39 @@ function getMaxSubSum(arr) {
   }
 }
 
+Переведите текст вида border - left - width в borderLeftWidth
+важность: 5
+Напишите функцию camelize(str), которая преобразует строки вида «my - short - string» в «myShortString».
+
+То есть дефисы удаляются, а все слова после них получают заглавную букву.
+
+  Примеры:
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+P.S.Подсказка: используйте split, чтобы разбить строку на массив символов, потом переделайте всё как нужно и методом join соедините обратно.
+
+Открыть песочницу с тестами для задачи.
+
+let str = "background-color";
+
+function splitToArr(str) {
+  let newArr = str.split(`-`);
+  console.log(newArr);
+  return newArr;
+}
+
+function firstLetterCap(elem) {
+  return elem[0].toUpperCase + elem.slice(1);
+}
+
+function joinCamel(arr) {
+  arr.map(firstLetterCap(elem))
+}
+
+str.splitToArr
+
+
+
+splitToArr(str)
--- main_book/96-113/script.js
+++ main_book/96-113/script.js
@@ -48,7 +48,7 @@ console.log(sum);
 Сформируйте с помощью двух вложенных циклов следующий массив:
 
 [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
-let arr =[];
+let arr = [];
 for (let i = 0; i < 3; i++) {
   arr[i] = [];
   for (let j = 1; j < 6; j++) {
@@ -57,14 +57,14 @@ for (let i = 0; i < 3; i++) {
 }
 console.log(arr)
 
-let arr =[];
+let arr = [];
 for (let i = 0; i < 3; i++) {
   arr[i] = [];
   for (let k = 0; k < 2; k++) {
     arr[i][k] = [];
-  for (let j = 1; j < 6; j++) {
-    arr[i][k].push(j);
-  }
+    for (let j = 1; j < 6; j++) {
+      arr[i][k].push(j);
+    }
   }
 }
 console.log(arr)
@@ -75,29 +75,29 @@ console.log(arr)
 Сформируйте с помощью трех вложенных циклов следующий массив:
 
 [
-	[
-		[1, 2, 3, 4, 5],
-		[1, 2, 3, 4, 5],
-	],
-	[
-		[1, 2, 3, 4, 5],
-		[1, 2, 3, 4, 5],
-	],
-	[
-		[1, 2, 3, 4, 5],
-		[1, 2, 3, 4, 5],
-	],
+  [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ],
+  [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ],
+  [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ],
 ]
 
 
 let arr = [];
 
 for (let i = 0; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 0; j <= 5; j++) {
-		arr[i][j] = j + 1;
-	}
+  arr[i] = [];
+
+  for (let j = 0; j <= 5; j++) {
+    arr[i][j] = j + 1;
+  }
 }
 
 console.log(arr);
@@ -106,11 +106,11 @@ console.log(arr);
 let arr = [];
 
 for (let i = 0; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 1; j <= 5; j++) {
-		arr[i].push(j);
-	}
+  arr[i] = [];
+
+  for (let j = 1; j <= 5; j++) {
+    arr[i].push(j);
+  }
 }
 
 console.log(arr);
@@ -119,11 +119,11 @@ console.log(arr);
 let arr = [];
 
 for (let i = 0; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 1; j <= 5; j++) {
-		arr.push(j);
-	}
+  arr[i] = [];
+
+  for (let j = 1; j <= 5; j++) {
+    arr.push(j);
+  }
 }
 
 console.log(arr);
@@ -131,11 +131,11 @@ console.log(arr);
 let arr = [];
 
 for (let i = 0; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 0; j <= 5; j++) {
-		arr[j][i] = j;
-	}
+  arr[i] = [];
+
+  for (let j = 0; j <= 5; j++) {
+    arr[j][i] = j;
+  }
 }
 
 console.log(arr);
@@ -148,9 +148,9 @@ console.log(arr);
 
 let arr = [];
 let counter = 1;
-for (let i =0; i<4; i++){
+for (let i = 0; i < 4; i++) {
   arr[i] = [];
-  for (let j =0; j<2; j++){
+  for (let j = 0; j < 2; j++) {
     arr[i][j] = counter++;
   }
 }
@@ -165,10 +165,10 @@ console.log(arr);
 
 let arr = [];
 let counter = 0;
-for (let i =0; i<4; i++){
+for (let i = 0; i < 4; i++) {
   arr[i] = [];
-  for (let j =0; j < 3; j++){
-    arr[i][j] = counter+=2;
+  for (let j = 0; j < 3; j++) {
+    arr[i][j] = counter += 2;
   }
 }
 console.log(arr);
@@ -180,14 +180,14 @@ console.log(arr);
 [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
 
 let arr = [];
-let counter =1;
-for (let i =0; i<2; i++){
+let counter = 1;
+for (let i = 0; i < 2; i++) {
   arr[i] = [];
-  for (let j =0; j < 2; j++){
+  for (let j = 0; j < 2; j++) {
     arr[i][j] = [];
-      for (let k =0; k < 2; k++){
-    arr[i][j][k] = counter++;
-  }
+    for (let k = 0; k < 2; k++) {
+      arr[i][j][k] = counter++;
+    }
   }
 }
 console.log(arr);
@@ -201,26 +201,130 @@ console.log(arr);
 let arr = [];
 
 for (let i = 0; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 0; j < 3; j++) {
-		arr[i][j] = k;
-		k++;
-	}
+  arr[i] = [];
+
+  for (let j = 0; j < 3; j++) {
+    arr[i][j] = k;
+    k++;
+  }
 }
 
 console.log(arr);
-Написанный код, однако, не делает задуманного. Найдите и исправьте ошибку автора.
+Написанный код, однако, не делает задуманного.Найдите и исправьте ошибку автора.
 
 K не определён
 let arr = [];
 
 for (let i = 0, k = 1; i < 3; i++) {
-	arr[i] = [];
-	
-	for (let j = 0; j < 3; j++,k++) {
-		arr[i][j] = k;
-	}
+  arr[i] = [];
+
+  for (let j = 0; j < 3; j++, k++) {
+    arr[i][j] = k;
+  }
 }
 
 console.log(arr);
+
+
+Многомерные обЪекты
+
+Задача 1⋕js.Pm.Md.MO.1
+
+Дан следующий объект:
+
+let obj = {
+  key1: {
+    key1: 1,
+    key2: 2,
+    key3: 3,
+  },
+  key2: {
+    key1: 4,
+    key2: 5,
+    key3: 6,
+  },
+  key3: {
+    key1: 7,
+    key2: 8,
+    key3: 9,
+  },
+}
+
+let sum = 0;
+sum = obj[`key1`][`key1`] + obj[`key1`][`key2`] + obj[`key1`][`key3`]; //  и тд
+console.log(sum)
+Найдите сумму элементов приведенного объекта.
+
+  Задача 2⋕js.Pm.Md.MO.2
+
+Дан следующий объект:
+
+let obj = {
+  1: {
+    1: 'a1',
+    2: 'a2',
+    3: 'a3',
+  },
+  2: {
+    1: 'b1',
+    2: 'b2',
+    3: 'b3',
+  },
+  3: {
+    1: 'c1',
+    2: 'c2',
+    3: 'c3',
+  },
+}
+
+console.log(obj[`2`][`2`]);
+console.log(obj[`3`][`1`]);
+Выведите на экран элемент 'b2' и элемент 'c1'.
+
+  Задача 3⋕js.Pm.Md.MO.3
+
+Дан следующий объект:
+
+let obj = {
+  key1: {
+    a: 1, b: 2, c: {
+      d: 3,
+      e: 4,
+    }, f: 5,
+  },
+  key2: {
+    g: 6, h: 7,
+  },
+}
+Найдите сумму всех элементов - чисел.
+
+  Задача 1⋕js.Pm.Md.MOI.1
+
+Дан следующий объект:
+
+let obj = {
+  1: {
+    1: 11,
+    2: 12,
+    3: 13,
+  },
+  2: {
+    1: 21,
+    2: 22,
+    3: 23,
+  },
+  3: {
+    1: 24,
+    2: 25,
+    3: 26,
+  },
+}
+// Используя циклы, найдите сумму элементов этого объекта.
+let sum = 0;
+for (let key in obj) {
+  const subElem = obj[key];
+  for (let subKey in subElem) {
+    sum += subElem[subKey]
+  }
+}
+console.log(sum);

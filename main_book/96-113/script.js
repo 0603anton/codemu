6e8c1cb585136d2daa6e915aff25d2ee13cfeb90
0603anton <zorgzb@gmail.com>
2022-07-16 22:27:39 +0200

    16/07

--- main_book/96-113/script.js
+++ main_book/96-113/script.js
@@ -328,3 +328,545 @@ for (let key in obj) {
   }
 }
 console.log(sum);
+
+Ключи из переменных в многомерных структурах JavaScript
+
+Задача 1⋕js.Pm.Md.VK.1
+
+Дан следующий объект с названиями месяцев:
+
+let months = {
+  'ru': [
+    'январь',
+    'февраль',
+    'март',
+    'апрель',
+    'май',
+    'июнь',
+    'июль',
+    'август',
+    'сентябрь',
+    'октябрь',
+    'ноябрь',
+    'декабрь',
+  ],
+  'en': [
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+    'july',
+    'august',
+    'september',
+    'october',
+    'november',
+    'december',
+  ],
+};
+// Даны также следующие переменные:
+
+let lang = 'ru'; // может быть или 'ru' или 'en'
+let month = 5;   // число от 0 до 11
+// Выведите на экран название месяца, соответствующее значениям переменных lang и month.
+
+console.log(months[lang][month]);
+
+Задача 2⋕js.Pm.Md.VK.2
+
+Дана вот такая структура для хранения списка дел за года, месяцы и дни:
+
+let affairs = {
+  '2018': {
+    11: {
+      29: ['дело111', 'дело112', 'дело113'],
+      30: ['дело121', 'дело122', 'дело123'],
+    },
+    12: {
+      30: ['дело211', 'дело212', 'дело213'],
+      31: ['дело221', 'дело222', 'дело223'],
+    },
+  },
+  '2019': {
+    12: {
+      29: ['дело311', 'дело312', 'дело313'],
+      30: ['дело321', 'дело322', 'дело323'],
+      31: ['дело331', 'дело332', 'дело333'],
+    }
+  },
+}
+
+const year = `2018`;
+const month = 11;
+const day = 29;
+
+
+// Пусть даны также 3 переменные, содержащие год, месяц и день.Выведите дело, соответствующее значениям переменных.
+console.log(affairs[year][month][day]);
+
+
+Задача 1⋕js.Pm.Md.MS.1
+
+Дан следующий массив работников:
+
+let employees = [
+  {
+    name: 'name1',
+    salary: 300,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+  },
+];
+// Выведите на экран сумму зарплат первого и второго работников.
+
+const sum = employees[0][`salary`] + employees[1][`salary`];
+console.log(sum);
+
+Задача 2⋕js.Pm.Md.MS.2
+
+Дан следующий объект со студентами:
+
+let students = {
+  'boys': ['Коля', 'Вася', 'Петя'],
+  'girls': ['Даша', 'Маша', 'Лена'],
+};
+// Выведите на экран элемент 'Вася' и элемент 'Лена'.
+
+console.log(students[`boys`][1])
+console.log(students[`girls`][2])
+
+Перебор многомерных комбинаций в JavaScript
+
+Задача 1⋕js.Pm.Md.MSI.1
+
+Дан следующий массив работников:
+
+let employees = [
+  {
+    name: 'name1',
+    salary: 300,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+  },
+];
+// Выведите на экран данные каждого работника в формате имя - зарплата.
+
+for (let elem of employees) {
+  console.log(`Name: ${elem['name']} Salary:${elem['salary']}`)
+}
+
+Задача 2⋕js.Pm.Md.MSI.2
+
+Дан следующий массив работников:
+
+let employees = [
+  {
+    name: 'name1',
+    salary: 300,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+  },
+];
+// Выведите на экран сумму зарплат всех работников.
+
+let sum = 0;
+for (let elem of employees) {
+  sum += elem[`salary`];
+}
+console.log(sum);
+
+  Задача 3⋕js.Pm.Md.MSI.3
+
+Дан следующий массив работников:
+
+let employees = [
+  {
+    name: 'name1',
+    salary: 300,
+    age: 28,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+    age: 29,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+    age: 30,
+  },
+  {
+    name: 'name4',
+    salary: 600,
+    age: 31,
+  },
+  {
+    name: 'name5',
+    salary: 700,
+    age: 32,
+  },
+];
+// Выведите на экран сумму зарплат тех работников, возраст которых равен или более 30 лет.
+let sum = 0;
+for (let elem of employees) {
+  if (elem.age >= 30) {
+    console.log(elem);
+    sum += elem.salary;
+  }
+}
+console.log(sum);
+
+  Задача 4⋕js.Pm.Md.MSI.4
+
+Дан следующий массив работников, в котором поле dismissed хранит в себе информацию о том, уволен работник или нет(true - уволен):
+
+let employees = [
+  {
+    name: 'name1',
+    salary: 300,
+    dismissed: false,
+  },
+  {
+    name: 'name2',
+    salary: 400,
+    dismissed: true,
+  },
+  {
+    name: 'name3',
+    salary: 500,
+    dismissed: false,
+  },
+  {
+    name: 'name4',
+    salary: 600,
+    dismissed: true,
+  },
+  {
+    name: 'name5',
+    salary: 700,
+    dismissed: false,
+  },
+];
+// Выведите на экран имена и зарплаты неуволенных работников.
+
+for (let elem of employees) {
+  if (!elem.dismissed) {
+    console.log(`Имя работника: ${elem.name} and his salary: ${elem.salary}`)
+  }
+}
+
+Задача 5⋕js.Pm.Md.MSI.5
+
+// Дана следующая структура с данными:
+
+let data = {
+  1: [
+    'data11',
+    'data12',
+    'data13',
+  ],
+  2: [
+    'data21',
+    'data22',
+    'data23',
+  ],
+  3: [
+    'data31',
+    'data32',
+    'data33',
+    'data34',
+    'data35',
+  ],
+  4: [
+    'data41',
+    'data42',
+  ],
+};
+// С помощью вложенных циклов выведите на экран все строки с данными.
+
+for (let elem in data) {
+  for (let arrElem of data[elem]) {
+    console.log(arrElem);
+  }
+}
+
+  Задача 6⋕js.Pm.Md.MSI.6
+
+Дана следующая структура с данными:
+
+let data = [
+  {
+    1: 'data11',
+    2: 'data12',
+    3: 'data13',
+    4: 'data14',
+  },
+  {
+    1: 'data21',
+    2: 'data22',
+    3: 'data33',
+  },
+  {
+    1: 'data31',
+    2: 'Anton',
+  },
+];
+// Количество объектов в массиве и количество элементов в объекте может быть произвольным.С помощью вложенных циклов выведите на экран все строки с данными.
+
+for (let elem of data) {
+  for (let subElem in elem) {
+    console.log(elem[subElem])
+  }
+}
+
+  Задача 7⋕js.Pm.Md.MSI.7
+
+Дана следующая структура с данными:
+
+let data = [
+  {
+    1: [
+      'data111',
+      'data112',
+      'data113',
+    ],
+    2: [
+      'data121',
+      'data122',
+      'data123',
+    ],
+  },
+  {
+    1: [
+      'data211',
+      'data212',
+      'data213',
+    ],
+    2: [
+      'data221',
+      'data222',
+      'data223',
+      'data224',
+    ],
+    3: [
+      'data231',
+      'data232',
+      'data233',
+      'data234',
+      'data235',
+    ],
+  },
+  {
+    1: [
+      'data411',
+      'data412',
+      'data413',
+    ],
+    2: [
+      'data421',
+    ],
+  },
+];
+// С помощью вложенных циклов выведите на экран все строки с данными.
+
+
+
+for (let elem of data) {
+  for (let key in elem) {
+    for (let subElem of elem[key]) {
+      console.log(subElem)
+    }
+  }
+}
+
+
+Задача 1⋕js.Pm.SM.Mth.1
+
+Возведите 2 в 10 степень.
+
+  console.log(Math.pow(2, 10));
+
+  Задача 2⋕js.Pm.SM.Mth.2
+
+Найдите квадратный корень из 245.
+
+console.log(Math.sqrt(245))
+
+Задача 3⋕js.Pm.SM.Mth.3
+
+Дан следующий массив:
+
+const arr = [4, 2, 5, 19, 13, 0, 10];
+// Найдите квадратный корень из суммы кубов его элементов.Для решения воспользуйтесь циклом for.
+
+let sum = 0;
+
+for (let elem of arr) {
+  sum += Math.pow(elem, 3);
+}
+
+console.log(Math.sqrt(sum))
+
+
+Задача 4⋕js.Pm.SM.Mth.4
+
+Найдите квадратный корень из 379. Результат округлите до целых, до десятых, до сотых.
+
+  console.log(Math.sqrt(379));
+console.log(Math.round(Math.sqrt(379)));
+console.log(Math.sqrt(379).toFixed(1));
+console.log(Math.sqrt(379).toFixed(2));
+
+  Задача 5⋕js.Pm.SM.Mth.5
+
+Найдите квадратный корень из 587. Округлите результат в большую и меньшую стороны, запишите результаты округления в объект с ключами 'floor' и 'ceil'.
+
+let number = Math.sqrt(587);
+console.log(number);
+console.log(Math.ceil(number));
+console.log(Math.floor(number));
+
+Задача 6⋕js.Pm.SM.Mth.6
+
+Даны числа 4, -2, 5, 19, -130, 0, 10. Найдите минимальное и максимальное число.
+
+  console.log(Math.max(4, -2, 5, 19, -130, 0, 10));
+console.log(Math.min(4, -2, 5, 19, -130, 0, 10));
+
+
+Задача 7⋕js.Pm.SM.Mth.7
+
+Выведите на экран случайное целое число от 1 до 100.
+
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomInt(1, 100));
+
+// Задача 8⋕js.Pm.SM.Mth.8
+
+// С помощью цикла заполните массив 10 - ю случайными целыми числами.
+
+let arr = [];
+let i = 0;
+while (i < 10) {
+  arr.push(getRandomInt(1, 100));
+  i++;
+}
+
+console.log(arr)
+
+
+Работа с регистром символов
+Изучите теорию по следующим ссылкам:
+
+toUpperCase toLowerCase
+
+Задача 1⋕js.Pm.SM.St.1
+
+Дана строка 'js'.Сделайте из нее строку 'JS'.
+
+let str = 'js';
+
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+  Задача 2⋕js.Pm.SM.St.2
+
+Дана строка 'JS'.Сделайте из нее строку 'js'.
+
+
+
+  Задача 3⋕js.Pm.SM.St.3
+
+Дана строка 'я учу javascript!'.Вырежьте из нее слово 'учу' и слово 'javascript' тремя разными способами(через substr, substring, slice).
+
+let str = 'я учу javascript!';
+
+console.log(str.substr(2, 3) + ` ` + str.substr(6));
+console.log(str.substring(2, 5) + ` ` + str.substr(6, 16));
+console.log(str.slice(2, 5) + ` ` + str.slice(6, 16));
+
+Задача 4⋕js.Pm.SM.St.4
+
+Дана строка 'abcde'.Определите позицию буквы 'c' в этой строке.
+
+const str = 'abcde';
+console.log(str.indexOf(`c`));
+
+  Задача 5⋕js.Pm.SM.St.5
+
+Дана строка.Проверьте, есть ли в этой строке символ 'a'.
+
+
+const str = 'abcde';
+console.log(str.indexOf(`c`));// if -1 => no
+
+  Задача 6⋕js.Pm.SM.St.6
+
+Дана строка.Проверьте, начинается ли эта строка с символа 'a'.
+
+const str = 'abcde';
+console.log(str.indexOf(`c`)); // if position 0
+  Задача 7⋕js.Pm.SM.St.7
+
+Дана строка.Проверьте, заканчивается ли эта строка на символ 'a'.
+const str = 'abcde';
+console.log(str.indexOf(`c`)); // добавить условие str.length == indexOf
+
+  Задача 8⋕js.Pm.SM.St.8
+
+Дана строка.Проверьте, начинается ли эта строка на 'http://'. // протос IndexOf ==0
+
+  Задача 9⋕js.Pm.SM.St.9
+
+Дана строка.Проверьте, заканчивается ли эта строка на '.html' // добавить условие str.length-4 == indexOf
+
+Работа с slice
+Изучите теорию по следующим ссылкам:
+
+slice
+
+Задача 5⋕js.Pm.SM.Ar.5
+
+Дан следующий массив:
+
+[1, 2, 3, 4, 5]
+Сделайте из этого массива следующий:
+
+[1, 2, 3]
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.slice(0, 3))
+Задача 6⋕js.Pm.SM.Ar.6
+
+Дан следующий массив:
+
+[1, 2, 3, 4, 5]
+Используя этот массив, запишите в новую переменную следующий массив:
+
+[4, 5]
+const arr = [1, 2, 3, 4, 5];
+const newArr = arr.slice(3);
+console.log(newArr)

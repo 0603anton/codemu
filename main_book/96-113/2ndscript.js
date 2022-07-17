f1a1165955fd76a07d0419508191c0f188dc2aa4
0603anton <zorgzb@gmail.com>
2022-07-17 23:07:38 +0200

    add 17/07

--- main_book/96-113/2ndscript.js
+++ main_book/96-113/2ndscript.js
@@ -129,3 +129,64 @@ function isSame(arr) {
 }
 
 console.log(isSame(arr));
+
+Сокращённые if и тд
+
+Задача 1⋕js.Pm.UF.LO.1
+
+Дана следующая функция:
+
+function func(a, b) {
+  if (a == b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+function func(a, b) {
+  return (a == b);
+} else {
+  return false;
+}
+}
+
+
+Перепишите ее код в сокращенной форме согласно изученной теории.
+
+  Задача 2⋕js.Pm.UF.LO.2
+
+Дана следующая функция:
+
+function func(a, b) {
+  return (a != b)
+} else {
+  return false;
+}
+}
+Перепишите ее код в сокращенной форме согласно изученной теории.
+
+  Задача 3⋕js.Pm.UF.LO.3
+
+Дана следующая функция:
+
+function func(a, b) {
+  return (a + b >= 10);
+} else {
+  return false;
+}
+}
+Перепишите ее код в сокращенной форме согласно изученной теории.
+
+  Задача 4⋕js.Pm.UF.LO.4
+
+Дана следующая функция:
+
+function func(num) {
+  return (num >= 0);
+} else {
+  return false;
+}
+}
+Перепишите ее код в сокращенной форме согласно изученной теории.

7c5d589e8a91f704aa8d904bd2a34ebaca77d151
0603anton <zorgzb@gmail.com>
2022-07-23 22:06:02 +0200

    23/07

--- main_book/96-113/2ndscript.js
+++ main_book/96-113/2ndscript.js
@@ -812,3 +812,143 @@ function randomElem(arr) {
 
 let arr = [1, 2, 3, 4, 5];
 console.log(randomElem(arr));
+
+function func() {
+	console.log('!');
+}
+func(); // выведет '!'
+console.log(func)
+alert(func)
+
+func = 'string'; // затрем переменную func
+console.log(func); // выведет 'string'
+
+let func1 = function () {
+  return 1;
+}
+
+let func2 = function () {
+  return 2;
+}
+
+console.log(func1() + func2())
+
+
+Массив с анонимными функциями
+
+let arr = [
+  function () {
+    return 1;
+  },
+  function () {
+    return 2;
+  },
+  function () {
+    return 3;
+  }
+];
+
+
+for (let func of arr){
+  console.log(func());
+}
+console.log(arr[2]())
+
+№1⊗jsPmFAFO
+
+Сделайте объект с тремя функциями. Пусть первая возвращает через return число 1, вторая - число 2, третья - число 3. С помощью созданных функций выведите в консоль сумму возвращаемых чисел.
+
+№2⊗jsPmFAFO
+
+Переберите созданный объект циклом и выведите результаты работы функций в консоль.
+
+let obj = {
+	func1: function() {return 1},
+  func2: function() {return 2},
+	func3: function() {return 3},
+};
+
+for (let elem in obj){
+  console.log(obj[elem]())
+}
+
+№3⊗jsPmFAFO
+
+Сделайте объект с тремя функциями, каждая из которых будет принимать параметром массив с числами. Сделайте так, чтобы первая функция возвращала сумму элементов массива, вторая функция - сумму квадратов, а третья - сумму кубов.
+
+let obj = {
+  getSum: function(arr){
+    let sum = 0;
+    for (let elem of arr){
+      sum+= elem;
+    }
+    return sum;
+  },
+    getSquareSum: function(arr){
+    let sum = 0;
+    for (let elem of arr){
+      sum+= Math.pow(elem,2);
+    }
+    return sum;
+  },
+    getCubeSum: function(arr){
+    let sum = 0;
+    for (let elem of arr){
+      sum+= Math.pow(elem,3);
+    }
+    return sum;
+  },
+}
+
+console.log(obj[`getSum`]([1,2,3]))
+
+
+№2⊗jsPmFAPFAP
+
+Сделайте функцию test, параметрами принимающую 3 функции и возвращающую сумму результатов переданных функций.
+
+function test(func1, func2, func3) {
+  return func1()+func2()+func3();
+}
+
+// №3⊗jsPmFAPFAP
+
+// Сделайте 3 функции, объявив их как Function Declaration и дав им имена func1, func2 и func3. Пусть первая функция возвращает 1, вторая - 2, а третья - 3. Передайте эти функции параметром в функцию test из предыдущей задачи.
+
+// function func1() {
+//   return 1;
+// }
+// function func2() {
+//   return 2;
+// }
+// function func3() {
+//   return 3;
+// }
+
+// console.log(test(func1, func2, func3));
+
+// №4⊗jsPmFAPFAP
+
+// Модифицируйте предыдущую задачу так, чтобы функции были объявлены как Function Expression с теми же именами.
+
+let func1 = function () {
+  return 1;
+}
+let func2 = function () {
+return 2;
+}
+let func3 = function () {
+  return 3;
+}
+
+console.log(test(func1, func2, func3));
+
+let func = function func (num,num1) {
+	return num + num1;
+};
+
+test(func);
+
+function test(func) {
+	console.log(func(2,3));
+}

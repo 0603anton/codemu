5eb8358cc253fc560dc28c63cca11b72edd48e6d
0603anton <zorgzb@gmail.com>
2022-08-07 22:28:50 +0200

    07/08

--- main_book/96-113/script.js
+++ main_book/96-113/script.js
@@ -870,3 +870,69 @@ console.log(arr.slice(0, 3))
 const arr = [1, 2, 3, 4, 5];
 const newArr = arr.slice(3);
 console.log(newArr)
+
+рекусрия 196
+
+№2⊗jsPmFARc
+
+Дан массив:
+
+let arr = [1, 2, 3, 4, 5];
+
+function getSqureSum (arr){
+  let sum = arr.shift()**2;
+  if (arr.length != 0){
+    sum += getSqureSum(arr);
+  }
+  return sum;
+}
+
+console.log(getSqureSum (arr))
+// С помощью рекурсии найдите сумму квадратов элементов этого массива.
+
+function getSum(arr) {
+	let sum = arr.shift();
+	console.log(`"Это первое значение sum"` + sum)
+	if (arr.length !== 0) {
+		sum += getSum(arr);
+    	console.log(`"Этозначение sum после ifa "` + sum)
+	}
+		console.log(`"Это sum который будет в ретёрне" ` + sum)
+	return sum;
+}
+
+console.log(getSum([1, 2, 3]));
+
+
+  // let counter =0;
+function numbers(params) {
+
+  if (params <10) {
+    return 1;
+  }
+// counter++;
+  
+  return 1 + numbers(params/10);
+}
+console.log(numbers(1000));
+
+
+const sumOfDigits = num => num / 10 < 1 ? 1 : 1 + sumOfDigits(num / 10);
+
+console.log(sumOfDigits(0))
+
+№1⊗jsPmFARS
+
+Дан многомерный объект произвольного уровня вложенности, например, такой:
+
+{a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}
+С помощью рекурсии выведите все примитивные элементы этого объекта в консоль.
+
+№2⊗jsPmFARS
+
+Дан многомерный массив произвольного уровня вложенности, например, такой:
+
+[1, [2, 7, 8], [3, 4, [5, [6, 7]]]]
+Напишите код, который развернет наш многомерный массив в одномерный. Для приведенного выше массива это будет выглядеть вот так:
+
+[1, 2, 7, 8, 3, 4, 5, 6, 7]

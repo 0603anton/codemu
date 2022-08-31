d2d2bc924de8c236cea0bd54ae75bd5c411f4ecf
0603anton <zorgzb@gmail.com>
2022-08-31 09:30:30 +0200

    30/08

--- LRJS/index.js
+++ LRJS/index.js
@@ -854,3 +854,57 @@ function getDateAgo(date, ago){
   let result = new Date(date.getFullYear(),date.getMonth(), date.getDate() - ago);
   return result;
 }
+
+
+
+Преобразуйте объект в JSON, а затем обратно в обычный объект
+важность: 5
+Преобразуйте user в JSON, затем прочитайте этот JSON в другую переменную.
+
+let user = {
+  name: "Василий Иванович",
+  age: 35
+};
+
+
+let jsonUser = JSON.stringify(user);
+console.log(jsonUser);
+let newUser = JSON.parse(jsonUser);
+console.log(newUser);
+
+
+
+Исключить обратные ссылки
+важность: 5
+В простых случаях циклических ссылок мы можем исключить свойство, из-за которого они возникают, из сериализации по его имени.
+
+Но иногда мы не можем использовать имя, так как могут быть и другие, нужные, свойства с этим именем во вложенных объектах. Поэтому можно проверять свойство по значению.
+
+Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup:
+
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  
+};
+
+// цикличные ссылки
+
+
+console.log( JSON.stringify(meetup, function replacer(key, value) {
+  // return (key != "" && value == meetup)? undefined: value;
+  return key value;
+  /* ваш код */
+}));
+
+/* в результате должно быть:
+{
+  "title":"Совещание",
+  "occupiedBy":[{"name":"Иванов"},{"name":"Петров"}],
+  "place":{"number":23}
+}
+*/

048cba1b954b9f70cd5ccd674748d80c074ff77d
0603anton <zorgzb@gmail.com>
2022-09-05 21:40:32 +0200

    05/09

--- LRJS/index.js
+++ LRJS/index.js
@@ -1176,3 +1176,57 @@ function inBetween(a,b){
     return x>=a && x<=b;
   }
 }
+
+Армия стрелков
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let shooter = function() { // функция shooter
+      console.log( i ); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+    i++;
+  }
+
+  return shooters;
+}
+
+let army = makeArmy();
+
+console.log(army[0]()); // у 0-го стрелка будет номер 10
+console.log(army[5]()); // и у 5-го стрелка тоже будет номер 10
+// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...
+
+
+// TODO
+Установка и уменьшение значения счётчика
+важность: 5
+Измените код makeCounter() так, чтобы счётчик мог увеличивать и устанавливать значение:
+
+counter() должен возвращать следующее значение (как и раньше).
+counter.set(value) должен устанавливать счётчику значение value.
+counter.decrease() должен уменьшать значение счётчика на 1.
+Посмотрите код из песочницы с полным примером использования.
+
+P.S. Для того, чтобы сохранить текущее значение счётчика, можно воспользоваться как замыканием, так и свойством функции. Или сделать два варианта решения: и так, и так.
+
+
+function makeCounter() {
+let count = 0;
+
+  function counter() {
+    return count++;
+  };
+
+  // counter.count = 0;
+  counter.set = value => count = value;
+  return counter;
+}
+
+let counter = makeCounter();
+console.log( counter() ); // 0
+console.log( counter() ); // 1
+counter.set(10)
+console.log( counter() ); // 1

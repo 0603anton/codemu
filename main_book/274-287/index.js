b3f617fb8263830c4f4b50eaccf5c26f1709c24c
0603anton <zorgzb@gmail.com>
2022-09-07 09:00:32 +0200

    07/09

--- main_book/274-287/index.js
+++ main_book/274-287/index.js
@@ -9,20 +9,20 @@
 
 // Дан инпут и кнопка. По нажатию на кнопку заблокируйте инпут.
 
-let elem = document.querySelector("#elem");
-let butt1 = document.querySelector(".b1");
-let butt2 = document.querySelector(".b2");
-let butt3 = document.querySelector(".b3");
+// let elem = document.querySelector("#elem");
+// let butt1 = document.querySelector(".b1");
+// let butt2 = document.querySelector(".b2");
+// let butt3 = document.querySelector(".b3");
 
-butt1.addEventListener(`click`, () => {
-  elem.disabled = true;
-});
-butt2.addEventListener(`click`, () => {
-  elem.disabled = false;
-});
-butt3.addEventListener(`click`, () => {
-  alert(elem.disabled);
-});
+// butt1.addEventListener(`click`, () => {
+//   elem.disabled = true;
+// });
+// butt2.addEventListener(`click`, () => {
+//   elem.disabled = false;
+// });
+// butt3.addEventListener(`click`, () => {
+//   alert(elem.disabled);
+// });
 
 // №2⊗jsPmFmDsb
 
@@ -31,3 +31,25 @@ butt3.addEventListener(`click`, () => {
 // №3⊗jsPmFmDsb
 
 // Дан инпут и кнопка. По нажатию на кнопку узнайте, заблокирован инпут или нет.
+
+// №1⊗jsPmFmChb
+
+// Дан чекбокс и две кнопки. По нажатию на первую кнопку установите чекбокс в отмеченное состояние, а по нажатию на вторую - в неотмеченное.
+// let elem = document.querySelector("#elem");
+// let butt1 = document.querySelector(".b1");
+// let butt2 = document.querySelector(".b2");
+
+// butt1.addEventListener(`click`, () => {
+//   if (elem.checked) {
+//     document.querySelector(`p`).textContent = `Hi!`;
+//   } else {
+//     document.querySelector(`p`).textContent = `Bye!`;
+//   }
+// });
+// butt2.addEventListener(`click`, () => {
+//   elem.checked = false;
+// });
+
+// №2⊗jsPmFmChb
+
+// Дан чекбокс, кнопка и абзац. По нажатию на кнопку, если чекбокс отмечен, выведите в абзац слово 'привет', а если чекбокс не отмечен, то слово 'пока'.

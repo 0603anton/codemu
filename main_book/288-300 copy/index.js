1b6b9144888cced026d39c5158b7e3016b69dbcb
0603anton <zorgzb@gmail.com>
2022-09-09 21:41:00 +0200

    09/09

---
+++ main_book/288-300 copy/index.js
@@ -0,0 +1,13 @@
+"use strict";
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const array = rangeOfNumbers(startNum, endNum - 1);
+
+    array.push(endNum);
+    return array;
+  }
+}
+
+console.log(countdown(4, 4));
